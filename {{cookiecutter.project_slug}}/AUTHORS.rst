2dbdd110b74ae221398727fdedd0bccdb57caa3f
Kathrin Dörfler <doerfler.kathrin@gmail.com>
2020-04-30 00:25:57 +0200

    authors updated

--- {{cookiecutter.project_slug}}/AUTHORS.rst
+++ {{cookiecutter.project_slug}}/AUTHORS.rst
@@ -2,5 +2,5 @@
 Authors
 =======
 
-* Gramazio Kohler Research `@gramaziokohler <https://github.com/gramaziokohler>`_
+* Augmented Fabrication Lab `@kathrindoerfler <https://github.com/augmentedfabricationlab>`_
 * {{cookiecutter.full_name}} <{{cookiecutter.email}}> `@{{cookiecutter.github_username}} <https://github.com/{{cookiecutter.github_username}}>`_
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__version__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__version__.py
@@ -2,9 +2,9 @@ __title__ = '{{cookiecutter.project_slug}}'
 __description__ = '{{cookiecutter.project_short_description}}'
 __url__ = 'https://github.com/{{cookiecutter.github_organization}}/{{cookiecutter.project_slug}}'
 __version__ = '{{cookiecutter.version}}'
-__author__ = 'Gramazio Kohler Research'
-__author_email__ = 'gramaziokohler@arch.ethz.ch'
+__author__ = 'Augmented Fabrication Lab'
+__author_email__ = 'doerfler@tum.de'
 __license__ = 'MIT license'
-__copyright__ = 'Copyright {% now 'utc', '%Y' %} Gramazio Kohler Research'
+__copyright__ = 'Copyright {% now 'utc', '%Y' %} Augmented Fabrication Lab'
 
 __all__ = ['__author__', '__author_email__', '__copyright__', '__description__', '__license__', '__title__', '__url__', '__version__']
